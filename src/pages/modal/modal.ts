366a011c01eae9c77df3a84649bb2cd0e3e598d4
Elisa Campos <elisacampospousa@gmail.com>
2017-06-21 12:02:47 +0200

    create a modal in mobile app

---
+++ src/pages/modal/modal.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavParams, ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the ModalPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-modal',
+  templateUrl: 'modal.html',
+})
+export class ModalPage {
+
+  constructor(private navParams: NavParams, private  view: ViewController) {
+  }
+
+  closeModal(){
+    this.view.dismiss();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ModalPage');
+  }
+
+}
